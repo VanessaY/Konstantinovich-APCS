1b15cc66ab9989e893bad5ebced23304c5467614
Vanessa <yu100997@gmail.com>
2014-02-25 00:39:38 -0500

    a

--- Knight/Driver.java
+++ Knight/Driver.java
@@ -1,7 +1,20 @@
 public class Driver{
-    public static void main(String[] args){
-	Knight k = new Knight(5);
-	k.solve();
-	System.out.println(k);
+    public static void main(String[]args){
+        try{
+            //this grabs the number when you run the command 'java Drive 5'
+            int size = Integer.parseInt(args[0]);
+
+            Knight t=new Knight(size);
+	    System.out.println(t);
+
+	    t.solve();//this prints the coordinate version of the solution
+
+            System.out.println(t);//this prints the board
+
+        }catch(Exception e){
+            //this means you didn't have a number after the command!
+            e.printStackTrace();
+            System.out.println("Correct usage:\njava Driver n\n where n is a positive integer.");
+        }
     }
 }
