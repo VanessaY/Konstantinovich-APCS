c9edebf3fe6eeb896188d7393cbde13be6cf32cc
Vanessa <yu100997@gmail.com>
2014-03-02 18:16:02 -0500

    a

---
+++ Merge/mergeSort.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+//a and b are sorted
+//smaller numbers into smaller indices
+
+public class mergeSort{
+    
+    public int[] merge(int[] a, int[] b){
+	int[] result = new int[a.length + b.length];
+	for (int i=0; i<max(a.length, b.length)){
+	    //check if a or b is empty
+	    
+
+}
