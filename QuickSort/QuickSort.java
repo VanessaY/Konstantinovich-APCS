55f8db5dc3b05e8c5dd7221e04937cc8bbf8a79e
Vanessa <yu100997@gmail.com>
2014-03-16 22:00:27 -0400

    a

--- QuickSort/QuickSort.java
+++ QuickSort/QuickSort.java
@@ -8,7 +8,15 @@ public class QuickSort{
     }
 
     public int quickSelect(int[] l, int k, int left, int right){
-	
+	int pivotI = partition(l, left, right);
+	//quest complete
+	if (pivotI == k)
+	    return l[k];
+	//try again
+	else if (k<pivotI)
+	    return quickSelect(l, k, left, pivotI-1);
+	else
+	    return quickSelect(l, k, pivotI+1, right);
     }
 
     public int partition(int[] l, int left, int right){
@@ -22,12 +30,35 @@ public class QuickSort{
 	int compare = l[pivot];
 	int leftint = left;
 	int rightint = right;
-
-	// 5, 6, 4, 8, 32, 1, 78, 5
-	while (leftint != rightint){
-	   
-
-	// return index of pivot
 	
+	//swap pivot and last obj in array
+	swap(l, pivot, l.length-1);
+
+	//swap, check from left
+	while (leftint != rightint + 1){
+	    if (l[left]>compare){
+		swap(l, left, right);
+		right++;
+	    }
+	    else{
+		left++;
+	    }
+	}
+	
+	//reinsert 
+	swap(l, l[l.length-1], l[right]);
     }
+
+    public void swap(int[] l, int a, int b){
+	int tmp = l[a];
+	l[a] = l[b];
+	l[b] = tmp;
+    }
+
+    public static void main(String[] args){
+	int[] l = {21,65,42,82,35,65,54,321,5,121,6531,16,351,341,213,1};
+	
+	this.partition(l
+    }
+       
 }
