3a36cb56aef2d1488a444611dbda12a4ec5b6570
Vanessa <yu100997@gmail.com>
2014-03-19 13:15:51 -0400

    a

---
+++ LinkedLists/Node.java
@@ -0,0 +1,32 @@
+public class Node{
+    private String data;
+    private Node next;
+
+    public Node(String data){
+	this.data = data;
+	next = null;
+    }
+
+    public void setNext(Node next){
+	this.next = next;
+    }
+
+    public Node getNext(){
+	return next;
+    }
+
+    public String getData(){
+	return data;
+    }
+
+    public void setData(String data){
+	this.data = data;
+    }
+
+    public String toString(){
+	return "Data: " + data;
+    }
+    
+    
+    
+}
